5fc72a15b75c5e0614892064c5d8be585d9fc0cc
Elaine Chen <amy8611020923@gmail.com>
2024-03-24 16:56:37 +0800

    set gitignore and some images

---
+++ src/pages/footer.jsx
@@ -0,0 +1,18 @@
+import './footer.css'
+
+
+function MyFooter ( ){
+
+    return(
+        <>
+        {/* <div className="text-white mt-24 "> */}
+        <div className="text-style">
+        Copyright © 2024 ElaineChen. All rights reserved.
+        </div>
+        </>
+    )
+
+}
+
+
+export default MyFooter
